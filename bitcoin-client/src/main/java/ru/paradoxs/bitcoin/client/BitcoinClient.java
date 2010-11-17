3edafc16ab95358df09a1838a64c9c3393c00cf3
matsh <matsh@c4deadc8-e444-49da-9d8c-c35d1ab4bd77>
2010-11-17 20:07:12 +0000

    Added Apache license JavaDoc to all Java files

--- bitcoin-client/src/main/java/ru/paradoxs/bitcoin/client/BitcoinClient.java
+++ bitcoin-client/src/main/java/ru/paradoxs/bitcoin/client/BitcoinClient.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2010 Aleksey Krivosheev (paradoxs.mail@gmail.com)
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package ru.paradoxs.bitcoin.client;
 
 import java.net.URI;
@@ -15,7 +30,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import ru.paradoxs.bitcoin.client.exceptions.BitcoinClientException;
 
-/** * 
+/**
  * @see http://www.bitcoin.org/wiki/doku.php?id=api
  * @author paradoxs
  */
